0a63434959755fae2d1cdec32a6dab5a21f577e8
wugang <o.wg@outlook.com>
2013-11-21 08:37:33 +0800

    xml data

---
+++ DynamicMap/Assets/Scripts/Helper/XMLHelper.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class XMLHelper : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load("E:\\test.xml");
+        foreach (XmlNode node in doc.GetElementsByTagName("packet"))
+            GetNodes(node);
+    }
+    static void GetNodes(XmlNode node)
+    {
+        XmlAttribute showname = node.Attributes["showname"];
+        XmlAttribute show = node.Attributes["show"];
+        XmlAttribute value = node.Attributes["value"];
+        if (showname != null || show != null || value != null)
+        {
+            //Console.Write("tagname:{0} ", node.Name);
+            //if (showname != null)
+            //    Console.Write("showname:{0} ", showname.Value);
+            //if (show != null)
+            //    Console.Write("show:{0} ", show.Value);
+            //if (value != null)
+            //    Console.Write("value:{0} ", value.Value);
+            //Console.WriteLine();
+        }
+        foreach (XmlNode xn in node.ChildNodes)
+            GetNodes(xn);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
